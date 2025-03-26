e735ac3e03df995e61d854967d9fda9849071fd3
Akhil K <akhilkarunakaran07@gmail.com>
2025-03-26 10:30:15 +0530

    Add files via upload

---
+++ armstrong.sh
@@ -0,0 +1,21 @@
+armstrong()
+{
+num=$1
+sum=0
+temp=$num
+digits=${#num}
+while [ $temp -gt 0 ];
+do
+digit=$((temp % 10))
+sum=$((sum + digit ** digits))
+temp=$((temp / 10))
+done
+if [ $sum -eq $num ];
+then
+echo "$num is an armstrong number"
+else
+echo "$num is not armstrong number"
+fi
+}
+read -p "Enter a number:" n
+armstrong $n
